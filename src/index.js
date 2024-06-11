99704667d67ce92ec895c2b5a753907dcf7e01ac
zarzani <zahraarzani@gmail.com>
2024-06-11 18:45:15 +0330

    importing product .jsx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import Product from "./component/product";
-import 'bootstrap/dist/css/bootstrap.min.css
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(<Product/>, document.getElementById('root'));
+
+ReactDOM.render(<Product/>, document.getElementById('root'));
 
 
 // If you want to start measuring performance in your app, pass a function
