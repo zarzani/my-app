07c5472247a06088dfb6e2fb97f0a68196f66d18
zarzani <zahraarzani@gmail.com>
2024-06-12 14:04:42 +0330

    color of button

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -1,16 +1,16 @@
-import { Button } from "bootstrap";
 import { Component } from "react";
 
 class Product extends Component {
     render() {
-        return(<div>
-        <span>product name</span>
-        <span>2</span>
-        <Button>+</Button>
-        <Button>-</Button>
-        <Button>delete</Button>
+        return (<div>
+            <span className="m-2 text-info">product name</span>
+            <span className="bagde bg-primary m-2">2</span>
+            <button type="button" className="btn btn-danger"> +</button>
+            <button type="button" className="btn btn-warning">-</button>
+
+            <button className="deleteBtn">delete</button>
         </div>);
-        
+
     }
 }
-export default Product;
+export default Product; 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 
 import reportWebVitals from './reportWebVitals';
 import Product from "./component/product";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -15,8 +14,7 @@ root.render(
   <Product/>
 );
 
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+reportWebVitals();
