e5bff581240588ad1af12f79b006e563c41ac976
zarzani <zahraarzani@gmail.com>
2024-06-12 14:14:42 +0330

    css

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -5,10 +5,10 @@ class Product extends Component {
         return (<div>
             <span className="m-2 text-info">product name</span>
             <span className="bagde bg-primary m-2">2</span>
-            <button type="button" className="btn btn-danger"> +</button>
-            <button type="button" className="btn btn-warning">-</button>
+            <button type="button" className="btn m-2 btn-danger"> +</button>
+            <button type="button" className="btn m-2 btn-warning">-</button>
 
-            <button className="deleteBtn">delete</button>
+            <button className="deleteBtn m-2">delete</button>
         </div>);
 
     }
